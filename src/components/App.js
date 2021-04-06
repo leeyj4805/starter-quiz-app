6c5020002a51da18c37540b9ddc3febee7f052f8
leeyj4805 <leeyj4805@gmail.com>
2021-04-06 20:48:18 +0900

    배포

--- src/components/App.js
+++ src/components/App.js
@@ -7,16 +7,28 @@ import { Quiz, Landing, Result, Loading } from "pages";
 import { QUIZZES } from "../constants";
 import { Helmet } from "react-helmet";
 import favicon from "assets/images/favicon.ico";
+import elon from "assets/images/elon.jpg";
 
 function App() {
 	const [score, setScore] = useState(0);
 	const convertedScore = Math.floor((score / QUIZZES.length) * 100);
+	const currentUrl = document.location.href;
 
 	return (
 		<ThemeProvider theme={theme}>
 			<Helmet>
 				<title>일론 머스크 지수 테스트하기</title>
 				<link rel="icon" href={favicon} />
+				<meta property="og:url" content={currentUrl} />
+				<meta property="og:title" content="일론 머스크 지수 테스트하기" />
+				<meta
+                    property="og:description"
+                    content="나는 일론 머스크에 대해서 얼마나 알고 있을까?"
+                />
+				<meta property="og:image" content={elon} />
+				<meta name="twitter:title" content="일론 머스크 지수 테스트하기" />
+                <meta name="twitter:description" content="나는 일론 머스크에 대해서 얼마나 알고 있을까?" />
+                <meta name="twitter:image" content={elon} />
 			</Helmet>
 			<GlobalStyle />
 			<Router>
